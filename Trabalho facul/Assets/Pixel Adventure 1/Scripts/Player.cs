268fa07c79b34f8d5122b8d0b0c53c4a201a001a
Srbottine <lucasbottine7@gmail.com>
2021-10-11 18:06:37 -0300

    mudanças no script

--- Trabalho facul/Assets/Pixel Adventure 1/Scripts/Player.cs
+++ Trabalho facul/Assets/Pixel Adventure 1/Scripts/Player.cs
@@ -6,14 +6,19 @@ public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    
+    public bool isJumping;
+    public bool doubleJump;
+
     private Rigidbody2D rig;
-    private bool isJumping;
-    private bool doubleJump;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    
     }
     // Update is called once per frame
     void Update()
@@ -25,6 +30,22 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+             anim.SetBool("walk", true);
+             transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+             anim.SetBool("walk", false);
+           
+        }
     }
     void Jump()
     {
@@ -34,12 +55,13 @@ public class Player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             }
             else
             {
-                  if(isJumping)
+                  if(doubleJump)
                   {
-                     rig.AddForce(new Vector2(0f, JumpForce * 2f), ForceMode2D.Impulse);
+                     rig.AddForce(new Vector2(0f, JumpForce *1f), ForceMode2D.Impulse);
                      doubleJump = false;
                  }
 
@@ -52,6 +74,7 @@ public class Player : MonoBehaviour
         if(collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
     void OnCollisionExit2D(Collision2D collision)
